1c824dc82effd06256d0ce6ef77ad4cf11b9309c
Srijit Saha <66782284+Srijit-Saha@users.noreply.github.com>
2023-03-14 23:15:40 +0530

    Delete Assignments_aml directory

--- Assignments_aml/aml_2_score.py
+++
@@ -1,6 +0,0 @@
-def score(text:str, model:sklearn.estimator,threshold:float):
-    prediction = False
-    propensity = 0.0
-    return prediction,propensity
-
-#use pretrained mlp to get prediction and propensity 
