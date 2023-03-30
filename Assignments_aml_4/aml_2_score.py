9dcf78db3465b7923a2d9692f009735132a5935d
Srijit Saha <66782284+Srijit-Saha@users.noreply.github.com>
2023-03-30 21:13:37 +0530

    Add files via upload

---
+++ Assignments_aml_4/aml_2_score.py
@@ -0,0 +1,6 @@
+def score(text:str, model:sklearn.estimator,threshold:float):
+    prediction = False
+    propensity = 0.0
+    return prediction,propensity
+
+#use pretrained mlp to get prediction and propensity 
